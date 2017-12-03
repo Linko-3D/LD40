2ed233a8075d09d1ae8d49563ad8fd2005c99e86
Angel Kyriako <angelkyriako@gmail.com>
2017-12-03 17:50:43 +0200

    princess cake model gizmos

--- Assets/Scripts/Character/PrincessCakeController.cs
+++ Assets/Scripts/Character/PrincessCakeController.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
 
 public class PrincessCakeController : MonoBehaviour, IWeightableController {
 
     public PrincessCakeModel.Settings Settings = new PrincessCakeModel.Settings();
-    
+
     public PrincessCakeModel Model { get; private set; }
 
     public string Name { get { return name; } }
@@ -12,8 +15,18 @@ public class PrincessCakeController : MonoBehaviour, IWeightableController {
         return Model;
     }
 
-    protected virtual void Start() {
+    protected void Start() {
         Model = new PrincessCakeModel(name, Settings);
     }
 
+#if UNITY_EDITOR
+    protected void OnDrawGizmosSelected() {
+        if (Model == null) return;
+
+        Handles.Label(transform.position, Name);
+        Handles.Label(transform.position + Vector3.down, "Weight: " + Model.Weight);
+        Handles.Label(transform.position + Vector3.down * 2, "CakesEaten: " + Model.CakesEaten);
+        Handles.Label(transform.position + Vector3.down * 3, "TeasDrunk: " + Model.TeasDrunk);
+    }
+#endif
 }
