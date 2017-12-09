380af59948e8ec72cd94a8bd0515012f2e73db0c
Angel Kyriako <angelkyriako@gmail.com>
2017-12-09 19:50:11 +0200

    static GameDataHolder autogen class to be used for webGL localization loading

--- Assets/Scripts/Managers/GameData.cs
+++ Assets/Scripts/Managers/GameData.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.IO;
+using System.Collections.Generic;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -23,30 +24,41 @@ public class GameData : SingletonMonobehaviour<GameData> {
     }
 
     public T LoadReadonly<T>(string path) {
-        return Load<T>(Path.Combine(Application.streamingAssetsPath, path + JSON_EXT));
+#if UNITY_WEBGL
+        return LoadFromHolder<T>(path);
+#else
+        return LoadFromFile<T>(Path.Combine(Application.streamingAssetsPath, path + JSON_EXT));
+#endif
     }
 
     public void SaveReadonly(string path, object data) {
-        Save(Path.Combine(Application.streamingAssetsPath, path), data + JSON_EXT);
+        SaveToFile(Path.Combine(Application.streamingAssetsPath, path), data + JSON_EXT);
     }
 
     public T LoadState<T>(string path) {
-        return Load<T>(Path.Combine(Application.persistentDataPath, path + JSON_EXT));
+        return LoadFromFile<T>(Path.Combine(Application.persistentDataPath, path + JSON_EXT));
     }
 
     public void SaveState(string path, object data) {
-        Save(Path.Combine(Application.persistentDataPath, path), data + JSON_EXT);
+        SaveToFile(Path.Combine(Application.persistentDataPath, path), data + JSON_EXT);
     }
 
-    protected T Load<T>(string filePath) {
+    protected T LoadFromFile<T>(string filePath) {
         _logger.Assert(File.Exists(filePath), "file: " + filePath + " does not exist.");
 
         string dataAsJson = File.ReadAllText(filePath);
-
         return JsonUtility.FromJson<T>(dataAsJson);
     }
 
-    protected void Save(string filePath, object data) {
+    protected T LoadFromHolder<T>(string path)
+    {
+        _logger.Assert(GameDataHolder.Data.ContainsKey(path), "file: " + path + " does not exist.");
+
+        string dataAsJson = GameDataHolder.Data[path];
+        return JsonUtility.FromJson<T>(dataAsJson);
+    }
+
+    protected void SaveToFile(string filePath, object data) {
         string dataAsJson = JsonUtility.ToJson(data);
         
         File.WriteAllText(filePath, dataAsJson);
@@ -58,17 +70,97 @@ namespace New.UTILITY {
     [CustomEditor(typeof(GameData))]
     [CanEditMultipleObjects]
     public class GameDataEditor : Editor {
-        private void OnEnable() {
 
+        private const string GAME_DATA_HOLDER_PATH = "Assets/Scripts/Managers/GameDataHolder.cs";
+        private const string GAME_DATA_LOCALIZATION_DIR = "Localization";
+
+        private GameData _gameData;
+
+        private void OnEnable()
+        {
+            _gameData = target as GameData;
+
+            _gameData.Initialize();
         }
 
         public override void OnInspectorGUI() {
             DrawDefaultInspector();
 
-#pragma warning disable 0219
-            GameData sGameData = target as GameData;
-#pragma warning restore 0219
+            EditorGUILayout.Space();
+
+            if (GUILayout.Button("Export Data Holder"))
+            {
+                ExportDataHolder();
+            }
+            GUILayout.Label("*Used for WebGL instead of retrieving the data via http.");
+        }
+
+        void ExportDataHolder()
+        {
+            Dictionary<string, string> allStreamingAssets = LoadStreamingAssets();
+            string dataHolderContent = GenerateGameDataHolder(allStreamingAssets);
+            File.WriteAllText(GAME_DATA_HOLDER_PATH, dataHolderContent);
+        }
+
+        Dictionary<string, string> LoadStreamingAssets()
+        {
+            Dictionary<string, string> allStreamingAssets = new Dictionary<string, string>();
+
+            string localizationDirPath = Path.Combine(Application.streamingAssetsPath, GAME_DATA_LOCALIZATION_DIR);
+
+            string[] locFilePaths = Directory.GetFiles(localizationDirPath);
+            foreach (string locFilePath in locFilePaths)
+            {
+                string locFileNameNoExtension = Path.GetFileNameWithoutExtension(locFilePath);
+                string locFileExtension = Path.GetExtension(locFilePath);
+                string locFileRelativePath = Path.Combine(GAME_DATA_LOCALIZATION_DIR, locFileNameNoExtension).Replace("\\", "\\\\");
+
+                if (locFileExtension == ".json")
+                {
+                    Debug.Log("Exporting streaming asset file: " + locFileRelativePath);
+
+                    string jsonContent = File.ReadAllText(locFilePath);
+
+                    string jsonStringified = jsonContent
+                                                .Replace("\"", "\\\"")
+                                                .Replace(System.Environment.NewLine, string.Empty);
+
+                    allStreamingAssets[locFileRelativePath] = jsonStringified;
+                }
+            }
+
+            return allStreamingAssets;
+        }
+
+        string GenerateGameDataHolder(Dictionary<string, string> streamingAssets)
+        {
+            string content = string.Empty;
+            content += "using System.Collections.Generic;\n";
+            content += "\n";
+            content += "public static class GameDataHolder {\n";
+            content += "\n";
+            content += "    public static Dictionary<string, string> Data = new Dictionary<string, string>() {\n";
+
+            int pathsLeft = streamingAssets.Count;
+            foreach (string path in streamingAssets.Keys)
+            {
+                --pathsLeft;
+                
+                if (pathsLeft != 0)
+                {
+                    content += "        { \"" + path + "\", \"" + streamingAssets[path] + "\"},\n";
+                }
+                else
+                {
+                    content += "        { \"" + path + "\", \"" + streamingAssets[path] + "\"}\n";
+                }
+            }
+            content += "    };\n";
+            content += "\n";
+            content += "}\n";
+            return content;
         }
     }
+
 #endif
 }
